7fa5008c9b7c26ea1d32a25bd4558b4ae23ff4c9
devxl <sharif.saleki@gmail.com>
2021-02-08 10:02:36 -0500

    lots of basic changes

--- code/fips.py
+++ code/fips.py
@@ -17,7 +17,7 @@ class FIPS:
             self,
             win,
             pos=(0.0, 0.0),
-            size=None,
+            size=10,
             ori=0.0,
             color=(0.0, 0.0, 0.0),
             contrast=1.0,
@@ -35,10 +35,11 @@ class FIPS:
         self.opacity = opacity
         self.name = name
         self.velocity = velocity
+        self._probes = None
+        self._frame = None
 
-        self.frame = self._generate_frame()
-
-    def _generate_frame(self):
+    @property
+    def frame(self):
         """
         Generates the square frame
 
@@ -46,58 +47,78 @@ class FIPS:
         -------
 
         """
+        if self._frame is None:
 
-        # corner coordinates
-        top_left = (self.pos[0] - self.size/2, self.pos[1] + self.size/2)
-        top_right = (self.pos[0] + self.size/2, self.pos[1] + self.size/2)
-        bot_left = (self.pos[0] - self.size/2, self.pos[1] - self.size/2)
-        bot_right = (self.pos[0] + self.size/2, self.pos[1] - self.size/2)
+            # corner coordinates
+            top_left = (self.pos[0] - self.size/2, self.pos[1] + self.size/2)
+            top_right = (self.pos[0] + self.size/2, self.pos[1] + self.size/2)
+            bot_left = (self.pos[0] - self.size/2, self.pos[1] - self.size/2)
+            bot_right = (self.pos[0] + self.size/2, self.pos[1] - self.size/2)
 
-        frame = visual.ShapeStim(
-                win=self.win,
-                lineWidth=5,
-                lineColor=self.color,
-                lineColorSpace='rgb',
-                fillColor=None,
-                fillColorSpace='rgb',
-                vertices=[top_left, top_right, bot_left, bot_right],
-                windingRule=None,
-                closeShape=True,
-                pos=self.pos,
-                size=self.size,
-                ori=self.ori,
-                opacity=self.opacity,
-                contrast=self.contrast,
-                depth=0,
-                interpolate=True,
-                name=self.name,
-                autoLog=False,
-                autoDraw=False
-        )
+            fr = visual.ShapeStim(
+                    win=self.win,
+                    lineWidth=5,
+                    lineColor=self.color,
+                    lineColorSpace='rgb',
+                    fillColor=None,
+                    fillColorSpace='rgb',
+                    vertices=[top_left, top_right, bot_left, bot_right],
+                    windingRule=None,
+                    closeShape=True,
+                    pos=self.pos,
+                    size=self.size,
+                    ori=self.ori,
+                    opacity=self.opacity,
+                    contrast=self.contrast,
+                    depth=0,
+                    interpolate=True,
+                    name=self.name,
+                    autoLog=False,
+                    autoDraw=False
+            )
 
-        return frame
+        return fr
 
-    def generate_probe(self, pos, color):
+    @property
+    def probes(self):
         """
-        Makes the probe that is supposed to flash inside the frame
+        Makes the probes that are supposed to flash inside the frame
 
         Returns
         -------
         """
-        probe = visual.GratingStim(
+        probes = dict()
+        top_pos = self.pos[1] + self.size/6 + self.size/12
+        bot_pos = (self.pos[1] - self.size/2) + self.size/6 + self.size/12
+        radius = self.size/6
+
+        probes["top"] = visual.GratingStim(
             win=self.win,
             mask='circle',
-            size=.8,
+            size=radius,
             phase = 0,
-            pos=pos,
+            pos=top_pos,
             sf=0,
             ori=0,
-            contrast=1,
-            color=color
+            contrast=self.contrast*.8,  # contrast 80% of the frame
+            color='blue'
         )
-        return probe
 
-    def move_frame(self, scr_frames, direction):
+        probes["bot"] = visual.GratingStim(
+            win=self.win,
+            mask='circle',
+            size=radius,
+            phase = 0,
+            pos=bot_pos,
+            sf=0,
+            ori=0,
+            contrast=self.contrast*.8,  # contrast 80% of the frame
+            color='red'
+        )
+
+        return probes
+
+    def move_frame(self, scr_frames, direction, transitions):
         """
         Oscillates the frame
 
@@ -109,17 +130,25 @@ class FIPS:
         direction : str
             "up", "down", "left", or "right"
         """
-        # velocity is given degrees/frame
-        for sf in range(scr_frames):
-            if direction == "left":
-                self.frame.pos -= (0, self.velocity)
-            elif direction == 'right':
-                self.frame.pos += (0, self.velocity)
-            elif direction == "up":
-                self.frame.pos += (self.velocity, 0)
-            elif direction == "down":
-                self.frame.pos -= (self.velocity, 0)
-            else:
-                raise ValueError("Direction should be one of 'up', 'down', 'left', or 'right'.")
+        # velocity is given in degrees/frame
+        flash_frames = 5
+        
+        for tr in transitions:
+            for sf in range(scr_frames):
+                if direction == "left":
+                    self.frame.pos -= (self.velocity, 0)
+                elif direction == 'right':
+                    self.frame.pos += (self.velocity, 0)
+                elif direction == "up":
+                    self.frame.pos += (0, self.velocity)
+                elif direction == "down":
+                    self.frame.pos -= (0, self.velocity)
+                else:
+                    raise ValueError("Direction should be one of 'up', 'down', 'left', or 'right'.")
 
-            self.frame.draw()
+                self.frame.draw()
+
+
+
+    def flash_probe(self):
+        pass
--- code/saccade_test.py
+++ code/saccade_test.py
@@ -5,31 +5,121 @@ Created at 2/6/21
 
 Testing script for saccade task
 """
-from psychopy import visual, gui, data, monitors
+from psychopy import visual, gui, data, monitors, core, logging
+from psychopy.iohub.client import yload, yLoader
+from psychopy.iohub import launchHubServer
 from fips import FIPS
 from psycphys import *
+from pathlib import Path
+from uuid import UUID
+import pandas as pd
+import sys
 
-# ===============
-# SETUP
-# ===============
+# ============================================================
+#                          SETUP
+# ============================================================
 
-# Monitor
-mon = make_test_monitor()
+# Experiment 
+# names 
+NAME = "FIPSSaccade"
+SESSIONS = 4
+PATH = Path('.').resolve() 
+sub_id = int(sys.argv[1])
+ses = sys.argv[2]
+
+# task
+if ses == "0":
+    task = "train"
+elif ses in ["1", "2"]:
+    task = "percept"
+elif ses in ["3", "4"]:
+    task = "saccade"
+else:
+    raise ValueError("Session number out of range.")
+
+# directories
+config_dir = PATH / "config"
+data_dir = PATH.parent / "data" 
+sub_id = f"{sub_id:02d}"
+sub_dir = data_dir / f"sub-{sub_id}"
+ses_dir = sub_dir / f"ses-{ses}"
+log_dir = ses_dir / "log"
+run_dir = ses_dir / "run"
+
+if not sub_dir.is_dir():
+    sub_dir.mkdir()
+ses_dir.mkdir()
+log_dir.mkdir()
+run_dir.mkdir()
+
+# files
+log_file = str(log_dir / f"sub-{sub_id}_ses-{ses}_task-{task}.log")
+run_file = str(run_dir / f"sub-{sub_id}_ses-{ses}_task-{task}.csv")
+
+# Info
+sub_dlg = gui.Dlg(title="Participant Information", labelButtonOK="Register", labelButtonCancel="Quit")
+
+# experiment
+sub_dlg.addText(text="Experiment", color="blue")
+sub_dlg.addFixedField("Title:", NAME)
+sub_dlg.addFixedField("Date:", str(data.getDateStr()))
+sub_dlg.addFixedField("Session:", ses)
+sub_dlg.addFixedField("Task:", task)
+
+# subject
+sub_dlg.addText(text="Participant info", color="blue")
+sub_dlg.addFixedField("ID:", sub_id)
+sub_dlg.addField("NetID:", tip="Leave blank if you do not have one")
+sub_dlg.addField("Initials:", tip="Lowercase letters separated by dots (e.g. g.o.d)")
+sub_dlg.addField("Age:", choices=list(range(18, 81)))
+sub_dlg.addField("Gender:", choices=["Male", "Female"])
+sub_dlg.addField("Handedness:", choices=["Right", "Left"])
+sub_dlg.addField("Vision:", choices=["Normal", "Corrected", "Other"])
+
+sub_params = {}
+sub_info = sub_dlg.show()
+if sub_dlg.OK:
+    sub_params["date"] = sub_info[1]
+    sub_params["netid"] = sub_info[5]
+    sub_params["initials"] = sub_info[6]
+    sub_params["age"] = int(sub_info[7])
+    sub_params["sex"] = sub_info[8]
+    sub_params["handedness"] = sub_info[9]
+    sub_params["vision"] = sub_info[10]
+
+else:
+    core.quit()
+
+# Display
+disp = make_test_monitor()
 
 # Window
 win = visual.Window(
     size=[1024, 768],
     fullscr=False,
     allowGUI=False,
-    monitor=mon,
+    monitor=disp,
     screen=0,
     units='deg',
     gamma=None,
     name='SaccadeWindow'
 )
 
-# Eye-tracker
-
-
 # Logging
+global_clock = core.Clock()
+logging.setDefaultClock(global_clock)
+logging.console.setLevel(logging.DEBUG)
+run_log = logging.LogFile(log_file, level=logging.DEBUG, filemode='w')
+logging.info(data.getDateStr())
 
+# Eye-tracker
+tracker_config = yload(open(str(config_dir / 'tracker_config.yaml'),'r'), Loader=yLoader)
+hub = launchHubServer(**tracker_config)
+tracker = hub.devices.tracker
+
+# ============================================================
+#                          PROCEDURE
+# ============================================================
+stim = FIPS(win=win)
+
+probe = stim.generate_probe()
